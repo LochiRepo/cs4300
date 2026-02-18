197eaf777ae7c4955e37ba21500142fc940f5e04
LochiRepo <lchinand@uccs.edu>
2026-02-17 23:29:14 -0700

    Update test_task4.py
    Forgot to update the function name

--- homework1/tests/test_task4.py
+++ homework1/tests/test_task4.py
@@ -2,12 +2,13 @@ import task4
 
 #Testing each variation, testing 10% on 100
 def test_discount_integers():
-    assert task4.calculate_discount(100, 10) == 90
+    assert task4.calcDisc(100, 10) == 90
 
 #testing for floats
 def test_discount_floats():
-    assert task4.calculate_discount(100.0, 15.0) == 85.0
+    assert task4.calcDisc(100.0, 15.0) == 85.0
 
 #Testing for values over 100
 def test_mixed_types():
-    assert task4.calculate_discount(200, 12.5) == 175.0
+    assert task4.calcDisc(200, 12.5) == 175.0
+
